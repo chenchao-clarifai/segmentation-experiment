17701bda32574ee1be0ac8e65ece6d8f42a1f056
Chenchao Zhao <chenchao.zhao@clarifai.com>
2022-06-04 17:25:27 -0400

    add tests

--- src/tests/test_datasets.py
+++ src/tests/test_datasets.py
@@ -4,3 +4,13 @@ from .. import datasets
 def test_base_dataset():
     dataset = datasets.SemanticSegmentationDataset("src/tests/data", "src/tests/data")
     assert len(dataset) == 1
+
+
+def test_transforms():
+    train_cfg = datasets.transforms.TrainTransformsConfigs()
+    eval_cfg = datasets.transforms.EvalTransformsConfigs()
+
+    datasets.transforms.get_train_transforms(train_cfg)
+    datasets.transforms.get_eval_transforms(eval_cfg)
+
+    # TODO: add some real images and test the transforms
