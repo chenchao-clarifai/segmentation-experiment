a2229d3c80a1a700b72649af83447a8a734e99c5
Chenchao Zhao <chenchao.zhao@clarifai.com>
2022-06-04 17:35:46 -0400

    test images

--- src/tests/test_datasets.py
+++ src/tests/test_datasets.py
@@ -1,3 +1,6 @@
+import numpy as np
+from PIL import Image
+
 from .. import datasets
 
 
@@ -10,7 +13,34 @@ def test_transforms():
     train_cfg = datasets.transforms.TrainTransformsConfigs()
     eval_cfg = datasets.transforms.EvalTransformsConfigs()
 
-    datasets.transforms.get_train_transforms(train_cfg)
-    datasets.transforms.get_eval_transforms(eval_cfg)
+    train_transforms = datasets.transforms.get_train_transforms(train_cfg)
+    eval_transforms = datasets.transforms.get_eval_transforms(eval_cfg)
 
-    # TODO: add some real images and test the transforms
+    image = Image.fromarray(
+        (np.random.rand(100, 100, 3) * 255).astype(np.uint8)
+    ).convert("RGB")
+    mask = Image.fromarray((np.random.rand(100, 100) * 255).astype(np.uint8)).convert(
+        "L"
+    )
+
+    train_transformed = train_transforms(image=image, mask=mask)
+    eval_transformed = eval_transforms(image=image, mask=mask)
+
+    assert train_transformed["image"].size() == (
+        3,
+        train_cfg.image_size,
+        train_cfg.image_size,
+    )
+    assert eval_transformed["image"].size() == (
+        3,
+        train_cfg.image_size,
+        train_cfg.image_size,
+    )
+    assert train_transformed["mask"].size() == (
+        train_cfg.image_size,
+        train_cfg.image_size,
+    )
+    assert eval_transformed["mask"].size() == (
+        train_cfg.image_size,
+        train_cfg.image_size,
+    )
